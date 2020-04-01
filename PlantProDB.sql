66115171ef9a8e49647fcf57144892359fe33d61
rudiejd <rudiejd@users.noreply.github.com>
2020-04-01 16:25:06 -0400

    Update PlantProDB.sql
    Change some syntax for MySQL

--- PlantProDB.sql
+++ PlantProDB.sql
@@ -1,16 +1,9 @@
-﻿
-USE master
-GO
 
-IF DB_ID('PlantProDB') IS NOT NULL	DROP DATABASE PlantProDB
-GO
+DROP DATABASE IF EXISTS PlantPro;
+CREATE DATABASE PlantPro;
+USE PlantPro;
 
-CREATE DATABASE PlantProDB
-GO
-
-USE [PlantProDB]
-
-CREATE TABLE [User]( 
+CREATE TABLE User( 
 	userId		int				NOT NULL,
 	email		varchar(30)		NOT NULL,
 	password	varchar(30)		NOT NULL,
@@ -19,7 +12,7 @@ CREATE TABLE [User](
 	location	point			NOT NULL,
 	isDeleted	bit				NOT NULL,
 	PRIMARY KEY (userId)
-)
+);
 
 CREATE TABLE Comment(
 	commentId			int				NOT NULL,
@@ -28,9 +21,9 @@ CREATE TABLE Comment(
 	body				varchar(120)	NOT NULL,
 	date				date			NOT NULL,
 	PRIMARY KEY (commentId),
-	FOREIGN KEY (userId) REFERENCES [User] (userId),
+	FOREIGN KEY (userId) REFERENCES User (userId),
 	FOREIGN KEY (plantSubmissionId) REFERENCES PlantSubmission (plantSubmissionId)
-) 
+); 
 
 CREATE TABLE PlantSubmission(
 	plantSubmissionId	int				NOT NULL, 
@@ -40,15 +33,16 @@ CREATE TABLE PlantSubmission(
 	location			point			NOT NULL, 
 	description			varchar(120)	NOT NULL,
 	PRIMARY KEY (plantSubmissionId),
-	FOREIGN KEY (userId) REFERENCES [User] (userId),
+	FOREIGN KEY (userId) REFERENCES User (userId),
 	FOREIGN KEY (plantId) REFERENCES Plant (plantId)
-)
+);
+
 
 CREATE TABLE PLANT(
 	plantId			int						NOT NULL,
 	division		varchar(64)				NOT NULL,
 	class			varchar(64)				NOT NULL,
-	[order]			varchar(64)				NOT NULL,
+	`order`			varchar(64)				NOT NULL,
 	family			varchar(64)				NOT NULL,
 	genus			varchar(64)				NOT NULL,
 	species			varchar(64)				NOT NULL,
